0af19781327f85cb1f6597486670d64853fc9da0
Herman Ryen Martinsen <hermanrm@vcvm18.idi.ntnu.no>
2022-02-17 17:07:31 +0100

    Change samples per gpu back to 1

--- cityscapes_train.sh
+++ cityscapes_train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./tools/dist_train.sh local_configs/segformer/B0/segformer.b0.1024x1024.city.160k.py 4
+./tools/dist_train.sh local_configs/segformer/B0/segformer.b0.1024x1024.city.160k.py 8
