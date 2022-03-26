91e94dd8a791e78cac257c2976a0e6ab900b5005
Herman Ryen Martinsen <hermanrm@vcvm307.idi.ntnu.no>
2022-03-26 16:05:11 +0100

    Add Lawin Transformer

--- mmseg/models/decode_heads/__init__.py
+++ mmseg/models/decode_heads/__init__.py
@@ -22,11 +22,12 @@ from .uper_head import UPerHead
 
 
 from .segformer_head import SegFormerHead
+from .lawin_head import LawinHead
 
 __all__ = [
     'FCNHead', 'PSPHead', 'ASPPHead', 'PSAHead', 'NLHead', 'GCHead', 'CCHead',
     'UPerHead', 'DepthwiseSeparableASPPHead', 'ANNHead', 'DAHead', 'OCRHead',
     'EncHead', 'DepthwiseSeparableFCNHead', 'FPNHead', 'EMAHead', 'DNLHead',
     'PointHead', 'APCHead', 'DMHead', 'LRASPPHead',
-    'SegFormerHead',
+    'SegFormerHead', 'LawinHead',
 ]
